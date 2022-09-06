114d8e659c077dce17ed0fb573afce597f930ed4
kyokio6002 <kyokio0725@gmail.com>
2022-09-06 23:31:07 +0900

    Fix: change type of progress_rate

--- toolkit/progressbar.py
+++ toolkit/progressbar.py
@@ -8,9 +8,9 @@ def show_progress_bar(index, file_path, max_size):
     max_progress_bar_width = 50
     progress_bar_width = min([terminal_width-25, max_progress_bar_width])
 
-    progress_rate = (index+1)//max_size
-    progress = progress_rate*progress_bar_width
-    progress_bar = '#'*(progress) + ' '*(progress_bar_width-progress_rate)
+    progress_rate = (index+1)/max_size
+    progress = int(progress_rate*progress_bar_width)
+    progress_bar = '#'*progress + ' '*(progress_bar_width-progress)
 
     file_name = os.path.basename(file_path)
     print("\r", f"[{progress_bar}] ({index+1}/{max_size}){file_name}", end="")
